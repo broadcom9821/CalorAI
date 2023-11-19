df9865f767c0ab8911217fa9232086c38ef90115
MYRADhub <myradism@gmail.com>
2023-11-19 02:33:38 -0500

    update for build

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import sys
-sys.path.append('../classification/')  # Replace with the actual path to the directory
+sys.path.append('./classification/')  # Replace with the actual path to the directory
 from foodClassify import make_prediction, load_model  # Import make_prediction from prediction_module
 from PIL import Image
 import io
@@ -17,9 +17,9 @@ async def predict(file: UploadFile = File(...)):
     # Process the image
     image = Image.open(io.BytesIO(await file.read()))
 
-    model = load_model('../classification/models/modelV2.pth')
+    model = load_model('./classification/models/modelV2.pth')
     # Make the prediction using the imported function
-    prediction_label = make_prediction(model, image, "../classification/food-101/classes.txt")
+    prediction_label = make_prediction(model, image, "./classification/food-101/classes.txt")
 
     # Return the prediction
     return JSONResponse(content={"label": prediction_label})
